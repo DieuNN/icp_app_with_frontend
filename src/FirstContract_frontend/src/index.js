d0b749e9f329e470965d0fd7f563256844aba125
dieu <nongngocdieu20122002@gmail.com>
2022-07-20 09:51:28 +0700

    Initial commit.

---
+++ src/FirstContract_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { FirstContract_backend } from "../../declarations/FirstContract_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await FirstContract_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
